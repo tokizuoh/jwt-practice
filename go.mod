9ad01aad289957802235e7b67bd187b08247a0b5
tokizuoh <tokizuoh@gmail.com>
2022-04-20 00:57:39 +0900

    JWT認証の実装を追加

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/tokizuoh/jwt-practice
+
+go 1.18
+
+require github.com/lestrrat-go/jwx/v2 v2.0.0-beta2
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/goccy/go-json v0.9.6 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
 type Model struct {
@@ -10,15 +17,71 @@ type Model struct {
 	Name string `json:"name"`
 }
 
+var stubModel = &Model{
+	Id:   1,
+	Name: "Oka",
+}
+
+var secret = `hi-mi-tsu`
+var globaKey *rsa.PrivateKey
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		stubModel := &Model{
-			Id:   1,
-			Name: "Oka",
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		t := jwt.New()
+		t.Set(jwt.IssuerKey, `tokizuoh/jwt-practice`)
+		t.Set(`private-claim-name`, secret)
+
+		key, err := rsa.GenerateKey(rand.Reader, 1024)
+		if err != nil {
+			errMsg := fmt.Sprintf("ERROR: %s", err)
+			http.Error(w, errMsg, http.StatusInternalServerError)
 		}
-		if err := json.NewEncoder(w).Encode(stubModel); err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		globaKey = key
+
+		signed, err := jwt.Sign(t, jwt.WithKey(jwa.RS384, key))
+		if err != nil {
+			errMsg := fmt.Sprintf("ERROR: %s", err)
+			http.Error(w, errMsg, http.StatusInternalServerError)
 		}
+		fmt.Fprintln(w, string(signed))
 	})
+
+	http.HandleFunc("/private", func(w http.ResponseWriter, r *http.Request) {
+		if globaKey == nil {
+			http.Error(w, "Unauthorized. Please access /token", http.StatusUnauthorized)
+			return
+		}
+
+		h := r.Header.Get("Authorization")
+		typ := "Bearer"
+		arr := strings.Split(h, " ")
+		if len(arr) != 2 && arr[0] != typ {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		rt := arr[1]
+
+		t, err := jwt.Parse([]byte(rt), jwt.WithKey(jwa.RS384, globaKey))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		v, ok := t.Get(`private-claim-name`)
+		if ok != true {
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+		if v == secret {
+			json.NewEncoder(w).Encode(stubModel)
+			return
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
