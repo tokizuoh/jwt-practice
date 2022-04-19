f533bbb12428eb7e6db7cdf04f87a02cf83c8fea
tokizuoh <tokizuoh@gmail.com>
2022-04-19 23:52:39 +0900

    ベースとなるレスポンスを追加

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Model struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
-	log.Println(1)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		stubModel := &Model{
+			Id:   1,
+			Name: "Oka",
+		}
+		if err := json.NewEncoder(w).Encode(stubModel); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
 }
